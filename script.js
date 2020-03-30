63f402ad81e68987058f82261f5b859af0f792be
rjagodzki <jagodzki.radek@outlook.com>
2020-03-30 20:09:23 +0200

    final version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://stark-sierra-58991.herokuapp.com/v1/task/';
+  const apiRoot = 'http://localhost:8080/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
@@ -38,9 +38,9 @@ $(document).ready(function() {
   function prepareBoardOrListSelectOptions(availableChoices) {
     return availableChoices.map(function(choice) {
       return $('<option>')
-          .addClass('crud-select__option')
-          .val(choice.id)
-          .text(choice.name || 'Unknown name');
+                .addClass('crud-select__option')
+                .val(choice.id)
+                .text(choice.name || 'Unknown name');
     });
   }
 
@@ -55,10 +55,10 @@ $(document).ready(function() {
       var $availableBoardsOptionElements = prepareBoardOrListSelectOptions(boards);
 
       $datatableRowEl.find('[data-board-name-select]')
-          .append($availableBoardsOptionElements);
+        .append($availableBoardsOptionElements);
 
       $datatableRowEl
-          .appendTo($tasksContainer);
+        .appendTo($tasksContainer);
     });
   }
 
